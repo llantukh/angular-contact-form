a08817bb0bc976342e2cb9d92ea63c3a3284f622
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-15 17:17:41 -0500

    wrote out all methods and got the form to submit to database

--- src/js/Controllers/contacts.js
+++ src/js/Controllers/contacts.js
@@ -1,23 +1,68 @@
-const SERVER_URL = 'https://class-server.herokuapp.com/collections/';
+const SERVER_URL = 'https://class-server.herokuapp.com/collections/lelas-contacts';
 
 function ContactController ($scope, $http) {
   $scope.contacts = [];
   $scope.errors = {};
 
-  function init () {};
-
+  function init () {
+    $http.get(SERVER_URL).then(function (resp) {
+      console.log(resp.data);
+      $scope.contacts = resp.data;
+    });
+  };
+  
   init();
 
-  $scope.validateName = function (name) {};
+  $scope.validateName = function (name) {
+    if (name === '') {
+      $scope.errors.name = "Name cannot be left empty."
+    } else {
+      $scope.errors.name = ''
+    }
+    return true
+  };
 
-  $scope.validateEmail = function (email) {};
+  $scope.validateEmail = function (email) {
+    if (!email.includes('@')) {
+      $scope.errors.email = "Email must include @"
+    } else {
+      $scope.errors.email = ''
+    }
+    if (email === '') {
+      $scope.errors.email = 'Email cannot be left empty.';
+    }
+    return true;
+  };
 
-  $scope.validateWebsite = function (website) {};
+  $scope.validateWebsite = function (website) {
+    if (!website.startsWith('http')) {
+      $scope.errors.website = "Website must start with http:// or https://."
+    } else {
+      $scope.errors.website = ''
+    }
+    if (website === '') {
+      $scope.errors.website = 'Website cannot be left empty.';
+    }
+    return true;
+  };
 
-  $scope.validateMessage = function (message) {};
+  $scope.validateMessage = function (message) {
+    if (message === '') {
+      $scope.errors.message = "Message cannot be left empty."
+    } else {
+      $scope.errors.message = ''
+    }
+    return true
+  };
 
-  $scope.addContact = function (contact) {};
-}
+  $scope.addContact = function (contact) {
+    $http.post(SERVER_URL, contact).then(function (resp) {
+      let contact = resp.data;
+      $scope.contacts.push(contact);
+      console.log($scope.contacts);
+    });
+  };
+};
 
 ContactController.inject = ['$scope','$http'];
 export { ContactController };
